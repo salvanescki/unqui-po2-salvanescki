436ab085997e9aef3bb02983fb4967bdee134d0a
salvanescki <salvanescki.unq@gmail.com>
2024-08-23 15:41:09 -0300

    Improve tests

--- test/ar/edu/unq/po2/tp2/EmpresaTest.java
+++ test/ar/edu/unq/po2/tp2/EmpresaTest.java
@@ -4,19 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.Month;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 class EmpresaTest {
 
-	private EmpleadoPlantaPermanente juan = new EmpleadoPlantaPermanente("Juan", "Calle falsa 123", "Casado", LocalDate.of(1978, Month.MAY, 2), 30000000, 2, 6);
-	private EmpleadoPlantaTemporaria carlos = new EmpleadoPlantaTemporaria("Carlos", "Calle falsa 890", "Soltero", LocalDate.of(2001, Month.NOVEMBER, 1), 25000000, LocalDate.of(2024, Month.DECEMBER, 1), 6);
+	private EmpleadoPlantaPermanente juan = new EmpleadoPlantaPermanente("Juan", "Calle falsa 123", "Casado",
+			LocalDate.of(1978, Month.MAY, 2), 30000000, 2, 6);
+	private EmpleadoPlantaTemporaria carlos = new EmpleadoPlantaTemporaria("Carlos", "Calle falsa 890", "Soltero",
+			LocalDate.of(2001, Month.NOVEMBER, 1), 25000000, LocalDate.of(2024, Month.DECEMBER, 1), 6);
 	private Empresa empresa = new Empresa("Empre S.A", 334608698233l);
 
 	@Test
-	void test() {
+	void totalSueldosNetosTest() {
 		empresa.contratarEmpleado(juan);
 		empresa.contratarEmpleado(carlos);
-		assertEquals(22548500, juan.sueldoNeto());
+		assertEquals(42564700, empresa.totalSueldosNetos());
+	}
+
+	@Test
+	void liquidacionDeSueldosTest() {
+		empresa.contratarEmpleado(juan);
+		empresa.contratarEmpleado(carlos);
+		empresa.liquidarSueldos();
+		assertTrue(true);
 	}
 
 }
