a6501afc744dcdb0e08ec4c8482b0062065c29bc
salvanescki <salvanescki.unq@gmail.com>
2024-08-27 17:17:25 -0300

    Add TP 3 item 9 Cuadrado implementation

---
+++ src/ar/edu/unq/po2/tp3/Cuadrado.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.po2.tp3;
+
+public class Cuadrado extends Rectángulo{
+	
+	public Cuadrado(Point puntoOrigen, int longitud) {
+		super(puntoOrigen, longitud, longitud);
+	}
+
+}
---
+++ test/ar/edu/unq/po2/tp3/CuadradoTestCase.java
@@ -0,0 +1,53 @@
+package ar.edu.unq.po2.tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuadradoTestCase {
+	
+	Cuadrado cuadradoA;
+	Cuadrado cuadradoB;
+	Point origen;
+	Point v;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		origen = new Point(0,0);
+		v = new Point(17, 20);
+		cuadradoA = new Cuadrado(origen, 3);
+		cuadradoB = new Cuadrado(v, 6);
+	}
+
+	@Test
+	void anchoTest() {
+		assertEquals(3, cuadradoA.ancho());
+		assertEquals(6, cuadradoB.ancho());	
+	}
+	
+	@Test
+	void alturaTest() {
+		assertEquals(3, cuadradoA.altura());
+		assertEquals(6, cuadradoB.altura());	
+	}
+	
+	@Test
+	void areaTest() {
+		assertEquals(9, cuadradoA.area());
+		assertEquals(36, cuadradoB.area());
+	}
+	
+	@Test
+	void perimetroTest() {
+		assertEquals(12, cuadradoA.perimetro());
+		assertEquals(24, cuadradoB.perimetro());
+	}
+	
+	@Test
+	void esHorizontalTest() {
+		assertFalse(cuadradoA.esHorizontal());
+		assertFalse(cuadradoB.esHorizontal());
+	}
+
+}
