c2918795db8f226bc0049ff385b53f4294430892
salvanescki <salvanescki.unq@gmail.com>
2024-08-27 10:09:55 -0300

    Setup TP 3-1

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Counter {
+	private List<Integer> numbers = new ArrayList<Integer>();
+
+	public void addNumber(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
