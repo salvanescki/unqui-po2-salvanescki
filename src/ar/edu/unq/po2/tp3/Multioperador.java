c54ba0a4b7d8da8a01bc45ed9bffb5fc258ca61d
salvanescki <salvanescki.unq@gmail.com>
2024-08-27 14:43:39 -0300

    Add unit tests for TP 3 item 6

---
+++ src/ar/edu/unq/po2/tp3/Multioperador.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Multioperador {
+
+	public static int sumar(List<Integer> numeros) {
+		return 0;
+	}
+	
+	public static int restar(List<Integer> numeros) {
+		return 0;
+	}
+	
+	public static int multiplicar(List<Integer> numeros) {
+		return 0;
+	}
+}
