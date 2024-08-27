8e41ce2ebbde3a796e9bd8f2c7a28eb814f48706
salvanescki <salvanescki.unq@gmail.com>
2024-08-27 16:50:48 -0300

    Add TP 3 item 8 solving

---
+++ src/ar/edu/unq/po2/tp3/Point.java
@@ -0,0 +1,34 @@
+package ar.edu.unq.po2.tp3;
+
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point() {
+		this.x = 0;
+		this.y = 0;
+	}
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void moveTo(int newX, int newY) {
+		this.x = newX;
+		this.y = newY;
+	}
+	
+	public Point add(Point other) {
+		return new Point(this.x + other.x, this.y + other.y);
+	}
+	
+	public int x() {
+		return this.x;
+	}
+	
+	public int y() {
+		return this.y;
+	}
+}
