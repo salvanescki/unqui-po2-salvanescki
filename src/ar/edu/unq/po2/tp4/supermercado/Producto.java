8bab13f6bc1fa7aa7872a7bfb784a62b03bddf62
salvanescki <salvanescki.unq@gmail.com>
2024-09-10 17:33:40 -0300

    initial commit tp 4

---
+++ src/ar/edu/unq/po2/tp4/supermercado/Producto.java
@@ -0,0 +1,35 @@
+package ar.edu.unq.po2.tp4.supermercado;
+
+import java.util.function.BooleanSupplier;
+
+public class Producto {
+
+	public Producto(String string, double d, boolean b) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Producto(String string, double d) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Double getPrecio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BooleanSupplier esPrecioCuidado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void aumentarPrecio(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ar/edu/unq/po2/tp4/supermercado/Supermercado.java
@@ -0,0 +1,24 @@
+package ar.edu.unq.po2.tp4.supermercado;
+
+public class Supermercado {
+
+	public Supermercado(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getCantidadDeProductos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void agregarProducto(Producto arroz) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Double getPrecioTotal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
