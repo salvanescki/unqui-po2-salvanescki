57ea3ffee809739cda8d355adb62ce6de25fb758
salvanescki <salvanescki.unq@gmail.com>
2024-09-10 17:33:40 -0300

    initial commit tp 4

---
+++ src/ar/edu/unq/po2/tp4/supermercado/ProductoPrimeraNecesidad.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tp4.supermercado;
+
+public class ProductoPrimeraNecesidad {
+
+	public ProductoPrimeraNecesidad(String string, double d, boolean b) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Double getPrecio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
