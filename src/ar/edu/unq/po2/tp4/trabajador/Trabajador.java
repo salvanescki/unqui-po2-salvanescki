d1d9a1a3e9f48a4c52cf2e25447aad50e433bf68
salvanescki <salvanescki.unq@gmail.com>
2024-09-10 21:11:39 -0300

    Add Trabajador tests

--- src/ar/edu/unq/po2/tp4/trabajador/Trabajador.java
+++ src/ar/edu/unq/po2/tp4/trabajador/Trabajador.java
@@ -1,5 +1,28 @@
 package ar.edu.unq.po2.tp4.trabajador;
 
-public class Trabajador {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Trabajador {
+	
+	private List<Ingreso> ingresosPercibidos = new ArrayList<Ingreso>();
+
+	public void agregarIngreso(Ingreso ingreso) {
+		//TODO Implementar
+	}
+	
+	public int getTotalPercibido() {
+		//TODO Implementar
+		return 0;
+	}
+	
+	public int getMontoImponible() {
+		//TODO Implementar
+		return 0;
+	}
+	
+	public int getImpuestoAPagar() {
+		//TODO Implementar
+		return 0;
+	}
 }
--- test/ar/edu/unq/po2/tp4/trabajador/TrabajadorTest.java
+++ test/ar/edu/unq/po2/tp4/trabajador/TrabajadorTest.java
@@ -12,11 +12,31 @@ class TrabajadorTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		juan = new Trabajador();
+		for(int i = 0; i < 12; i++) {
+			String mes = String.valueOf(i);
+			juan.agregarIngreso(new Ingreso(mes, "Sueldo Mes: " + mes, 25000000));
+		}
+		for(int i = 0; i < 3; i++) {
+			juan.agregarIngreso(new Ingreso("Diciembre", "Horas extras Diciembre", 360000));
+		}
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getTotalPercibidoTest() {
+		// 300,000,000 + 1,080,000 = 301,080,000
+		assertEquals(301080000, juan.getTotalPercibido());
+	}
+	
+	@Test
+	void getMontoImponibleTest() {
+		// 300,000,000
+		assertEquals(300000000, juan.getMontoImponible());
+	}
+	
+	@Test
+	void getImpuestoAPagarTest() {
+		// 300,000,000 * 0.02 = 6,000,000
+		assertEquals(6000000, juan.getImpuestoAPagar());
 	}
 
 }
