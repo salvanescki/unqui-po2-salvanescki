329b0ffb6270f3829152566168cfded25c10a23f
salvanescki <salvanescki.unq@gmail.com>
2024-09-11 00:30:31 -0300

    Test classes and Fix errors

--- src/ar/edu/unq/po2/tp5/caja1/ProductoCooperativa.java
+++ src/ar/edu/unq/po2/tp5/caja1/ProductoCooperativa.java
@@ -2,16 +2,22 @@ package ar.edu.unq.po2.tp5.caja1;
 
 public class ProductoCooperativa extends Producto{
 
-	private double descuento;
+	private static final double DESCUENTO = 0.1;
+	private final double descuento;
 
 	public ProductoCooperativa(String nombre, int stock, int precio, double descuento) {
 		super(nombre, stock, precio);
 		this.descuento = descuento;
 	}
 	
+	public ProductoCooperativa(String nombre, int stock, int precio) {
+		super(nombre, stock, precio);
+		this.descuento = DESCUENTO;
+	}
+	
 	@Override
 	public int getPrecio() {
-		return (int) (this.precio * descuento);
+		return (int) (this.precio * (1.0 - descuento));
 	}
 
 }
