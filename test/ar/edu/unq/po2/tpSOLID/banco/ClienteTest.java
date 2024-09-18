a93c81f7fac8b6b5914cbdcc4f1615c031bd16b1
salvanescki <salvanescki.unq@gmail.com>
2024-09-18 12:52:07 -0300

    Add tests for TDD

---
+++ test/ar/edu/unq/po2/tpSOLID/banco/ClienteTest.java
@@ -0,0 +1,31 @@
+package ar.edu.unq.po2.tpSOLID.banco;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClienteTest {
+	
+	private Cliente roberto;
+	private Banco bancoProvincia;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		bancoProvincia = new Banco();
+		roberto = new Cliente("Roberto", "Rodriguez", "Calle falsa 123", 29, 60000000);
+	}
+
+	@Test
+	void ingresosAnualesTest() {
+		assertEquals(720000000, roberto.ingresosAnuales());
+	}
+	
+	@Test
+	void solicitarCreditoTest() {
+		assertEquals(0, roberto.getAhorros());
+		bancoProvincia.solicitarCredito(roberto, 1500000000);
+		assertEquals(1500000000, roberto.getAhorros());
+	}
+
+}
